327ae42e3044584f8a57aba76211bc2614c373b9
Tom Jackman <tom.dls@hotmail.com>
2018-11-29 18:04:51 +0000

    add class colouring definitions

---
+++ assets/deps/classColours.js
@@ -0,0 +1,214 @@
+var r3eClassColours = {
+  classes: {
+    253: {
+      Name: "FRJ Cup",
+      colour: "rgba(0, 167, 83, 0.8)",
+      Id: 253
+    },
+    255: {
+      Name: "Aquila CR1 Cup",
+      colour: "rgba(243, 217, 72, 0.8)",
+      Id: 255
+    },
+    1685: {
+      Name: "Hillclimb Icons",
+      colour: "rgba(28, 201, 199, 0.8)",
+      Id: 1685
+    },
+    1687: {
+      Name: "GTR 1",
+      colour: "rgba(216, 53, 8, 0.8)",
+      Id: 1687
+    },
+    1703: {
+      Name: "GTR 3",
+      colour: "rgba(0, 154, 223, 0.8)",
+      Id: 1703
+    },
+    1704: {
+      Name: "GTR 2",
+      colour: "rgba(44, 181, 82, 0.8)",
+      Id: 1704
+    },
+    1706: {
+      Name: "German Nationals",
+      colour: "rgba(180, 152, 1, 0.8)",
+      Id: 1706
+    },
+    1708: {
+      Name: "Group 5",
+      colour: "rgba(62, 176, 228, 0.8)",
+      Id: 1708
+    },
+    1710: {
+      Name: "GTR X",
+      colour: "rgba(38, 167, 162, 0.8)",
+      Id: 1710
+    },
+    1712: {
+      Name: "Touring Classics",
+      colour: "rgba(255, 120, 0, 0.8)",
+      Id: 1712
+    },
+    1713: {
+      Name: "GTO Classics",
+      colour: "rgba(227, 49, 31, 0.8)",
+      Id: 1713
+    },
+    1714: {
+      Name: "P1",
+      colour: "rgba(231, 64, 64, 0.8)",
+      Id: 1714
+    },
+    1717: {
+      Name: "Silhouette Series",
+      colour: "rgba(67, 66, 64, 0.8)",
+      Id: 1717
+    },
+    1921: {
+      Name: "DTM 2013",
+      colour: "rgba(13, 39, 80, 0.8)",
+      Id: 1921
+    },
+    1922: {
+      Name: "WTCC 2013",
+      colour: "rgba(0, 75, 130, 0.8)",
+      Id: 1922
+    },
+    1923: {
+      Name: "P2",
+      colour: "rgba(72, 100, 222, 0.8)",
+      Id: 1923
+    },
+    2322: {
+      Name: "BL Cup",
+      colour: "",
+      Id: 2322
+    },
+    2378: {
+      Name: "Procar",
+      colour: "rgba(0, 168, 223, 0.8)",
+      Id: 2378
+    },
+    2922: {
+      Name: "ADAC GT Masters 2013",
+      colour: "rgba(0, 154, 223, 0.8)",
+      Id: 2922
+    },
+    3086: {
+      Name: "DTM 2014",
+      colour: "rgba(13, 39, 80, 0.8)",
+      Id: 3086
+    },
+    3375: {
+      Name: "ADAC GT Masters 2014",
+      colour: "rgba(0, 154, 223, 0.8)",
+      Id: 3375
+    },
+    3499: {
+      Name: "DTM 1992",
+      colour: "rgba(27, 65, 159, 0.8)",
+      Id: 3499
+    },
+    3905: {
+      Name: "WTCC 2014",
+      colour: "rgba(45, 196, 242, 0.8)",
+      Id: 3905
+    },
+    4260: {
+      Name: "DTM 2015",
+      colour: "rgba(13, 39, 80, 0.8)",
+      Id: 4260
+    },
+    4516: {
+      Name: "ADAC GT Masters 2015",
+      colour: "rgba(0, 154, 223, 0.8)",
+      Id: 4516
+    },
+    4517: {
+      Name: "WTCC 2015",
+      colour: "rgba(38, 85, 162, 0.8)",
+      Id: 4517
+    },
+    4597: {
+      Name: "FR2 Cup",
+      colour: "rgba(7, 168, 192, 0.8)",
+      Id: 4597
+    },
+    4680: {
+      Name: "Audi Sport TT Cup 2015",
+      colour: "rgba(90, 104, 114, 0.8)",
+      Id: 4680
+    },
+    4813: {
+      Name: "NSU TTS Cup",
+      colour: "rgba(48, 51, 144, 0.8)",
+      Id: 4813
+    },
+    4867: {
+      Name: "Tatuus F4 Cup",
+      colour: "rgba(128, 179, 57, 0.8)",
+      Id: 4867
+    },
+    5234: {
+      Name: "Audi TT RS cup",
+      colour: "rgba(62, 63, 58, 0.8)",
+      Id: 5234
+    },
+    5262: {
+      Name: "DTM 2016",
+      colour: "rgba(13, 39, 80, 0.8)",
+      Id: 5262
+    },
+    5383: {
+      Name: "FR US Cup",
+      colour: "rgba(11, 76, 117, 0.8)",
+      Id: 5383
+    },
+    5385: {
+      Name: "KTM X-Bow RR Cup",
+      colour: "rgba(234, 91, 36, 0.8)",
+      Id: 5385
+    },
+    5634: {
+      Name: "Mercedes-AMG Motorsport",
+      colour: "rgba(35, 131, 169, 0.8);",
+      Id: 5634
+    },
+    5652: {
+      Name: "FR3 Cup",
+      colour: "rgba(6, 130, 245, 0.8)",
+      Id: 5652
+    },
+    5726: {
+      Name: "Audi Sport TT Cup 2016",
+      colour: "rgba(199, 18, 44, 0.8)",
+      Id: 5726
+    },
+    5824: {
+      Name: "FR X-17 Cup",
+      colour: "rgba(232, 0, 0, 0.8)",
+      Id: 5824
+    },
+    6036: {
+      Name: "WTCC 2016",
+      colour: "rgba(0, 87, 184, 0.8)",
+      Id: 6036
+    },
+    6172: {
+      Name: "eSports WTCC",
+      colour: "rgba(234, 91, 12, 0.8)",
+      Id: 6172
+    },
+    6309: {
+      Name: "WTCC 2017",
+      colour: "rgba(0, 87, 184, 0.8)",
+      Id: 6309
+    },
+    6344: {
+      Name: "BMW M235i Racing Cup",
+      colour: "rgba(109, 162, 212, 0.8)",
+      Id: 6344
+    }
+  }
+}
