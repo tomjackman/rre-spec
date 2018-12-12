0e90b87f8563b2a8308e7590aa4dc88b26bc5599
Tom Jackman <tom.dls@hotmail.com>
2018-12-12 21:36:02 +0000

    add safety car alerts

---
+++ assets/components/widgets/safetyCarDeployed/safetyCarDeployed.jsx
@@ -0,0 +1,8 @@
+UI.widgets.SafetyCarDeployed = React.createClass({
+	render: function() {
+		var self = this;
+		return (
+			<div className="safetyCarDeployed animated flash infinite delay-1s">Safety Car</div>
+		);
+	}
+});
---
+++ assets/components/widgets/safetyCarIn/safetyCarIn.jsx
@@ -0,0 +1,15 @@
+UI.widgets.SafetyCarIn = React.createClass({
+  componentWillUnmount: function() {
+    // hide safety car indicator when dismounting
+		UI.state.activeWidgets.SafetyCarDeployed.active = false;
+	},
+  render: function() {
+		var self = this;
+		return (
+      <div className="safetyAlert animated slideInRight">
+        <div className="raceControl">Race Control</div>
+  			<div className="safetyCarIn">Safety Car in this lap</div>
+      </div>
+		);
+	}
+});
