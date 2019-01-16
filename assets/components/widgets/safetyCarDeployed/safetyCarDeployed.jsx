969bd39b96e2acc5534826583abcd900cd3e7dad
Tom Jackman <tom.dls@hotmail.com>
2019-01-16 19:07:11 +0000

    add new alerts styling

--- assets/components/widgets/safetyCarDeployed/safetyCarDeployed.jsx
+++ assets/components/widgets/safetyCarDeployed/safetyCarDeployed.jsx
@@ -2,7 +2,9 @@ UI.widgets.SafetyCarDeployed = React.createClass({
 	render: function() {
 		var self = this;
 		return (
-			<div className="safetyCarDeployed animated flash infinite">Safety Car</div>
+			<div className="safetyCarDeployedContainer">
+				<div className="safetyCarDeployed animated flash infinite">Safety Car</div>
+			</div>
 		);
 	}
 });
