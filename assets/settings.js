b910a98cfc2f9cc310ed91818001addd49824e5c
Tom Jackman <tom.dls@hotmail.com>
2019-10-08 20:26:53 +0100

    v1.9.0 version bumps

--- assets/settings.js
+++ assets/settings.js
@@ -1,7 +1,7 @@
 var settings = {
 	"offline": false, // turns on or off offline mode
 	"teamEvent": false, // tweaks overlay to suit team races
-	"language": "english" // choose a language, english, chinese, russian etc. Supported can be seen in the 'languages' folder in the main directory.
+	"language": "english" // choose a language, english, french, chinese, russian etc. Supported can be seen in the 'languages' folder in the main directory.
 }
 
 if (typeof module !== 'undefined') {
